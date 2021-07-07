39e013a459b81b028e106f365b1c003481cf5e96
Syrine Joulak <syrinejoulak@gmail.com>
2021-07-07 16:37:15 +0200

    adding menu burger

--- src/shared/MainNavigation/MainNavigation.js
+++ src/shared/MainNavigation/MainNavigation.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../images/logo aleia.png";
 import menu from "../images/Burger Menu.png";
 
+import Backdrop from "../UIElements/Backdrop";
+import SideDrawer from "./SideDrawer";
+
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
   return (
-    <div className="main-navigation">
-      <img src={logo} alt="logo" className="logo"></img>
-      <div className="nav">
-        <ul className="nav-list">
-          <li>About us</li>
-          <li>Services</li>
-          <li>Careers</li>
-          <li>Functions</li>
-        </ul>
+    <React.Fragment>
+      <div className="main-navigation">
+        <img src={logo} alt="logo" className="logo"></img>
+        <div className="nav">
+          <ul className="nav-list">
+            <li>About us</li>
+            <li>Services</li>
+            <li>Careers</li>
+            <li>Functions</li>
+          </ul>
+        </div>
+        <div className="nav-opt">
+          <button className="nav-btn">Contact us</button>
+          <h1 className="nav-language">Fr</h1>
+        </div>
+        <img
+          src={menu}
+          alt="menu-burger"
+          className="menu"
+          onClick={openDrawerHandler}
+        ></img>
       </div>
-      <div className="nav-opt">
-        <button className="nav-btn">Contact us</button>
-        <h1 className="nav-language">Fr</h1>
-      </div>
-      <img src={menu} alt="logo" className="menu"></img>
-    </div>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <ul className="nav-list__side-drawer">
+            <li>About us</li>
+            <li>Services</li>
+            <li>Careers</li>
+            <li>Functions</li>
+          </ul>
+        </nav>
+      </SideDrawer>
+    </React.Fragment>
   );
 };
 
---
+++ src/shared/MainNavigation/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { CSSTransition } from "react-transition-group";
+
+import ReactDOM from "react-dom";
+
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => {
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-right"
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className="side-drawer" onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+};
+
+export default SideDrawer;
